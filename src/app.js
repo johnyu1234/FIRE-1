1278de6dd3b7d838b66927089241c7ba4bbffc9e
johnyu1234 <johnyu@hotmail.my>
2025-02-08 16:16:03 +0800

    To deploy on Netlify

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const setRoutes = require('./routes/index');
-
+const serverless = require('serverless-http');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Set up routes
-setRoutes(app);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
 });
+
+// Export the app as a serverless function
+module.exports.handler = serverless(app);
